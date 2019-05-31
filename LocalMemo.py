e32fdf66d5e2674a282eeab8d1db248b4eb095d7
PlasticMandarin <48742402+PlasticMandarin@users.noreply.github.com>
2019-05-31 16:47:39 +0900

    Create LocalMemo.py
    로컬 메모 모듈

---
+++ LocalMemo.py
@@ -0,0 +1,15 @@
+"""
+로컬 메모
+"""
+
+print("새 메모를 입력하세요: ") #새로운 내용을 입력 받는다.
+memo = input()
+
+f = open("새파일.txt", 'a') #입력받은 값을 새 파일에 저장한다.
+f.write(memo)
+f.write('\n')
+f.close()
+
+f2 = open("새파일.txt", 'r') #저장된 내용을 출력한다.
+print(f2.read())
+f2.close()
