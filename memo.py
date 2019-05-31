b4d784a07989ef75ebc70c3cbb95b34c6c34dfcf
PlasticMandarin <48742402+PlasticMandarin@users.noreply.github.com>
2019-05-31 16:33:11 +0900

    Update memo.py
    메모 기능

--- memo.py
+++ memo.py
@@ -1,15 +1,17 @@
+#파이썬 메모
+
 import sys
 
 option = sys.argv[1]
 
-if option =='-a':
+if option =='-a': #메모 입력
     memo = sys.argv[2]
     f= open('memo.txt','a')
     f.write(memo)
     f.write('\n')
     f.close()
 
-elif option=='-v':
+elif option=='-v': #메모 출력
     f = open('memo.txt')
     memo=f.read()
     f.close()
